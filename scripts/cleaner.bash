e969ac67d1aa5eb65adf719033c082c2df42deb4
Alan Szpigiel <alanszp@gmail.com>
2016-08-20 20:04:55 -0300

    add run command to spark

--- scripts/cleaner.bash
+++ scripts/cleaner.bash
@@ -10,11 +10,16 @@ function sbt () {
     run_command "sbt ${*}"
 }
 
+function cleaner () {
+    run_command "spark-submit --class org.edu.utn.newspark.lsa.LSA target/scala-2.11/lsa.jar"
+}
+
 function help() {
     out '
 <33>Available methods:<0>
   <32;1>c|command<0>                Run a command in the container. (Eg: /bin/bash).<0>
   <32;1>s|sbt<0>                    Run a SBT command. (Eg: run).<0>
+  <32;1>r|run<0>                    Execute spark.<0>
 '
 }
 
@@ -28,6 +33,10 @@ function run () {
         s|sbt)
             shift;
             sbt $*
+            exit 0
+        ;;
+        r|run)
+            cleaner
             exit 0
         ;;
         *)
